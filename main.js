394e73ef9aca2b9717d4ad638c768accaf30c3b5
Miguel Diaz <118069844+mikeworldyt@users.noreply.github.com>
2023-10-23 16:42:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
+async function getDirectory(){ 
+  let res=await fetch("./index.json")
+  let directory=await res.json()
+  console.log(directory)
+}
 
-const $ulList = document.getElementById("list");
-
-const $columnDivNotLevel2 = Array.from(
-  document.querySelectorAll(".column")
-).filter((el) => el.id !== "Level2");
-
-
-$ulList.addEventListener("pointerdown", (e) => {
-
-  if (e.target.nodeName === "A") {
-    $columnDivNotLevel2.forEach((el) => {
-      const $iframe = el.querySelector("iframe");
-      if ($iframe !== null) {
-        $iframe.src = "";
-      }
-    });
-  }
-});
-
-
-
+getDirectory()
