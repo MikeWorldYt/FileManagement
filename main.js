6f0b4a8ea50c72002c0eab62440d6354ddf27be2
mikeworldyt <diazmig98@gmail.com>
2023-10-25 21:59:25 -0600

    Oct25.1

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-var iframe = document.getElementById("lvl2");
-var column = document.getElementById("column");
+async function getJSON() { 
+    let res = await fetch("./index.JSON");
+    let directory = await res.json();
+    console.log(directory);
+}
 
-iframe.onload = function() {
-    if (iframe.contentDocument.body.innerHTML === ""){
-        column.style.display = "none";
-    } 
-    else{
-        column.style.display = "block";
-    }
-};
+getJSON()
+function createListDirectory(keys) {
+    const $ul = document.createElement("ul");
+    $ul.append()
+}
+
+const ColumnFill = document.getElementById(Level2).innerHTML="ul";
+console.log(ColumnFill);
+
+
+
+// var iframe = document.getElementById("lvl2");
+// var column = document.getElementById("column");
+
+//iframe.onload = function() {
+//    if (iframe.contentDocument.body.innerHTML === ""){
+//        column.style.display = "none";
+//    } 
+//    else{
+//        column.style.display = "block";
+//    }
+//};
